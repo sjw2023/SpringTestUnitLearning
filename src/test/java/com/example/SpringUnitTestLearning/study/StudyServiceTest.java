7012d7cabe8ef6cc1b63e1ca10133edd5f723620
JoowonSuh <sjw2023@gmail.com>
2022-12-03 17:02:43 +0900

    move @Mock as parameter of test method

--- src/test/java/com/example/SpringUnitTestLearning/study/StudyServiceTest.java
+++ src/test/java/com/example/SpringUnitTestLearning/study/StudyServiceTest.java
@@ -2,16 +2,17 @@ package com.example.SpringUnitTestLearning.study;
 
 import com.example.SpringUnitTestLearning.member.MemberService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@ExtendWith(MockitoExtension.class)
 class StudyServiceTest {
-    @Mock MemberService memberService;
-    @Mock StudyRepository studyRepository;
 
     @Test
-    void createStudyService(){
+    void createStudyService(@Mock MemberService memberService, @Mock StudyRepository studyRepository ){
         StudyService studyService = new StudyService( memberService, studyRepository);
         assertNotNull(studyService);
     }
